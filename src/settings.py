d3a89f69ad5aa5ecf0c791a7316649f1b6bf649c
ANGEL.ALVAREZ <angel.alvarez.pascua@gmail.com>
2024-12-25 10:03:52 +0100

    first commit

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@
 num_tests = 3
 
 # List of different row sizes to test on
-all_num_rows = [1_000_000, 5_000_000, 25_000_000, 50_000_000, 75_000_000, 100_000_000]
+all_num_rows = [1_000_000, 5_000_000, 25_000_000, 50_000_000]
 
 # Number of clients to simulate (1 million clients)
 num_clients = 1_000_000
